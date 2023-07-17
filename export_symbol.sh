9efe5da47290a9a9e7f6e0db1231af1bed6a45eb
alokgupt <alokgupt@xilinx.com>
2023-07-16 19:30:05 -0700

    Update export_symbol.sh

--- export_symbol.sh
+++ export_symbol.sh
@@ -1,7 +1,7 @@
-export MALI_LIBS=/scratch/ZCU102_PL/ZynqMP/build/tmp/sysroots/plnx_aarch64/usr/lib
-export MALI_INCLUDE=/scratch/ZCU102_PL/ZynqMP/build/tmp/sysroots/plnx_aarch64/usr/include
-export ARM_CROSS_COMPILER_PATH=/proj/petalinux/petalinux-v2017.1_daily_latest/petalinux-v2017.1-final/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-g++
-export ARM_CROSS_COMPILER_PATH_C=/proj/petalinux/petalinux-v2017.1_daily_latest/petalinux-v2017.1-final/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc
+export MALI_LIBS=<PATH to SYSROOT/usr/lib>/sysroots/plnx_aarch64/usr/lib
+export MALI_INCLUDE=<PATH to SYSROOT/usr/include>/sysroots/plnx_aarch64/usr/include
+export ARM_CROSS_COMPILER_PATH=<PATH to Cross compiler g++>/aarch64-linux-gnu/bin/aarch64-linux-gnu-g++
+export ARM_CROSS_COMPILER_PATH_C=<PATH to Cross compiler gcc>/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc
 export CXXFLAGS=-DENABLE_FBDEV
 
 
