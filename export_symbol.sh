ad8a93588e1884dc1b3df35d39b7b17eedc18120
Alok Gupta <alokgupt@xilinx.com>
2023-07-18 12:41:23 -0700

    Update export_symbol.sh

--- export_symbol.sh
+++ export_symbol.sh
@@ -1,4 +1,4 @@
-export MALI_LIBS=/scratch/ZCU102_PL/ZynqMP/build/tmp/sysroots/plnx_aarch64/usr/lib
+export MALI_LIBS=<Path to>/sysroots/plnx_aarch64/usr/lib
 export MALI_INCLUDE=/scratch/ZCU102_PL/ZynqMP/build/tmp/sysroots/plnx_aarch64/usr/include
 export ARM_CROSS_COMPILER_PATH=/proj/petalinux/petalinux-v2017.1_daily_latest/petalinux-v2017.1-final/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-g++
 export ARM_CROSS_COMPILER_PATH_C=/proj/petalinux/petalinux-v2017.1_daily_latest/petalinux-v2017.1-final/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc
